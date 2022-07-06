eabec1c3c1224f22df8ace863c018e7702186d93
Rick Tesmond <ricktesmond@Ricks-MBP.lan>
2022-07-06 16:58:21 -0500

    login/reg complete

--- vibraille/vibraille_services/models.py
+++ vibraille/vibraille_services/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Note(models.Model):
+    """Model to create a notes object."""
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    ascii_text = models.TextField(default='')
+    braille_format = models.TextField(default='')
+
+    class Meta:
+        ordering = ['created']
--- vibraille/vibraille_services/views.py
+++ vibraille/vibraille_services/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets, permissions, generics, renderers
+from .serializers import UserSerializer, VBTokenObtainPairSerializer, RegisterSerializer
+from rest_framework.permissions import AllowAny
+from .models import Note
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        # 'users': reverse('user-list', request=request, format=format),
+        # 'notes': reverse('notes-list', request=request, format=format)
+    })
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+class VBObtainTokenPairView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+    serializer_class = VBTokenObtainPairSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
