61a82809e80b3bc7bc0703f983ef3be69f994c84
Rick Tesmond <ricktesmond@Ricks-MBP.lan>
2022-07-20 17:46:57 -0500

    update settings

--- vibraille/settings.py
+++ vibraille/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k7op(f80#u1^r!17u9%0nzcfb#-@d$12ek@j^2!+8_92fol1vm'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
